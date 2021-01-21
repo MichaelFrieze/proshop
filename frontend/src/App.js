c7b3141869404d98875c6c834462dfa2b58b54f5
Michael Frieze <michaelfrieze@icloud.com>
2021-01-21 17:19:04 -0500

    installed bootstrap and lux theme from bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome To Proshop</h1>
+      <Header />
+      <main>
+        <h1>WELCOME TO PROSHOP</h1>
+      </main>
+      <Footer />
     </>
   )
 }
