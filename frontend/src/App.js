50cb329261cca961d05b7fca39d1038de62a5819
Michael Frieze <michaelfrieze@icloud.com>
2021-01-22 19:50:31 -0500

    front end | homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>WELCOME TO PROSHOP</h1>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,15 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
