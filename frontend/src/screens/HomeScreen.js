e8c0297bdcfe0b501751300bda05921d045f9b1c
Michael Frieze <michaelfrieze@icloud.com>
2021-01-22 20:49:21 -0500

    front end | rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
@@ -8,7 +9,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
